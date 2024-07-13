e070e7a4dbcac56591c393f874abae2daa62e032
Bhagyashri <bhagyashris500@gmail.com>
2024-07-14 00:21:00 +0530

    record_bet_two_dates

--- src/main/java/com/krushiSevaCenter/dao/CustomerHistoryDao.java
+++ src/main/java/com/krushiSevaCenter/dao/CustomerHistoryDao.java
@@ -1,10 +1,14 @@
 package com.krushiSevaCenter.dao;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.krushiSevaCenter.dto.ProductInvoiceDTO;
 import com.krushiSevaCenter.entity.CustomerBill;
 import com.krushiSevaCenter.entity.CustomerHistory;
 
@@ -15,4 +19,24 @@ public interface CustomerHistoryDao  extends CrudRepository<CustomerHistory, Lon
 	List<CustomerHistory> findAll();
 
 	void save(CustomerBill customerBill);
+
+   /* @Query("SELECT new com.krushiSevaCenter.dto.ProductInvoiceDTO(ch.productId, p.productName, SUM(ch.quantity), ch.date) " +
+            "FROM CustomerHistory ch " +
+            "JOIN Product p ON ch.productId = p.id " +
+            "WHERE ch.date BETWEEN :fromDate AND :toDate " +
+            "GROUP BY ch.productId, p.productName, ch.date")
+     List<ProductInvoiceDTO> findInvoicesByDateRange(@Param("fromDate") LocalDate fromDate, @Param("toDate") LocalDate toDate);
+*/
+	
+	@Query("SELECT new com.krushiSevaCenter.dto.ProductInvoiceDTO(ch.productId, p.productName, SUM(ch.quantity), MAX(ch.date)) " +
+		       "FROM CustomerHistory ch " +
+		       "JOIN Product p ON ch.productId = p.id " +
+		       "WHERE ch.date BETWEEN :fromDate AND :toDate " +
+		       "GROUP BY ch.productId, p.productName")
+		List<ProductInvoiceDTO> findInvoicesByDateRange(@Param("fromDate") LocalDate fromDate, @Param("toDate") LocalDate toDate);
+
+	
 }
+
+
+
