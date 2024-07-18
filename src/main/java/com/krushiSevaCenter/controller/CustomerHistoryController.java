d87216a61e82ccac58ec8a2dbda1ee35dc3fdb87
swarajghadage <148381059+swarajghadage@users.noreply.github.com>
2024-07-18 12:07:45 +0530

    return product to supplier

--- src/main/java/com/krushiSevaCenter/controller/CustomerHistoryController.java
+++ src/main/java/com/krushiSevaCenter/controller/CustomerHistoryController.java
@@ -65,6 +65,7 @@ public ResponseEntity<String> addCustomerHistory(@RequestBody BillRequestDto dto
     
     
     
+    
     @GetMapping("/returnPolicySupplier")
     public String showReturn(Model model) {
         List<Product> products = proservice.getAllProducts();
--- src/main/java/com/krushiSevaCenter/service/CustomerHistoryService.java
+++ src/main/java/com/krushiSevaCenter/service/CustomerHistoryService.java
@@ -97,6 +97,7 @@ public class CustomerHistoryService {
         }
     }
     
+    
 
     
     public List<ProductInvoiceDTO> findInvoicesByDateRange(LocalDate fromDate, LocalDate toDate) {
